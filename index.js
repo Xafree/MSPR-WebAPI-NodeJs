2d021a9ac653cf697001638a44695ffbc478e078
Clovis <clovis.sargenton@gmail.com>
2022-01-14 12:11:13 +0100

    add GET /prospects and POST /prosppect

--- index.js
+++ index.js
@@ -1,36 +1,55 @@
 const express = require('express');
 const jsforce = require('jsforce');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 const app = express();
 const PORT = 3001;
 
-const {SF_LOGIN_URL,SF_USERNAME,SF_PASSWORD,SF_TOKEN} = process.env;
+const { SF_LOGIN_URL, SF_USERNAME, SF_PASSWORD, SF_TOKEN } = process.env;
 const connect = new jsforce.Connection({
-    loginUrl : SF_LOGIN_URL
+    loginUrl: SF_LOGIN_URL
 })
 
-connect.login(SF_USERNAME,SF_PASSWORD+SF_TOKEN, (err,userInfo)=>{
-    if(err){
-        console.error(err)
-    } else{
-        console.log("User Id : "+ userInfo.id)
-        console.log("Org Id : "+ userInfo.organizationId)
-    }
-} );
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get('/', (req, res)=>{
-    connect.query("Select ID, Name FROM Account", (err, result) =>{
-        if(err){
+connect.login(SF_USERNAME, SF_PASSWORD + SF_TOKEN, (err, userInfo) => {
+    if (err) {
+        console.error(err)
+    } else {
+        console.log("User Id : " + userInfo.id)
+        console.log("Org Id : " + userInfo.organizationId)
+    }
+});
+
+
+app.get('/prospects', (req, res) => {
+    connect.query("Select First_Name__c, Email__c, Name FROM Prospect__c", (err, result) => {
+        if (err) {
             res.send(err);
-        }else{
-            console.log("Totale recods "+ result.totalSize);
+        } else {
+            console.log("Totale recods " + result.totalSize);
             res.json(result.records);
         }
     })
-
 })
 
-app.listen(PORT, ()=>{
+
+app.post('/prospect', (req, res) => {
+    connect.sobject('Prospect__c').create({
+        'First_Name__c': req.body.firstName,
+        'Email__c': req.body.email
+    }).then((result) => {
+        res.status(201).send(result);
+    }).catch((err) => {
+        res.status(422).send(err);
+    })
+})
+
+
+
+
+app.listen(PORT, () => {
     console.log(`Serveur is running at http://localhost:${PORT}`);
 })
 
